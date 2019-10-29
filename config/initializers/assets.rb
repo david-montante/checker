9ebf793e20c74ed74b8239839725dba8249415bb
david-montante <davidmontante@gmail.com>
2019-10-29 17:34:36 -0600

    Adds email styles

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,8 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+Rails.application.config.assets.precompile += %w( custom.css )
 Rails.application.config.assets.precompile += %w( styles.css )
 
 # Precompile additional assets.
