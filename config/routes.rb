f312e4317ff5d067cfc0a89c3ac8a909c862e7e5
david-montante <davidmontante@gmail.com>
2019-10-29 17:05:37 -0600

    Styles devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  resources :users do
+    collection do
+      get 'dashboard',        to: 'dashboard#index'
+    end
+  end
+
   root 'home#index'
 end
